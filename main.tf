00dac2c6905ec3e76062d55c0cdc2b2ebf1cbe73
omerteomim <omerteomim@gmail.com>
2025-06-27 16:39:16 +0300

    change integration method

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_apigatewayv2_api" "lambda_api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id             = aws_apigatewayv2_api.lambda_api.id
   integration_type   = "AWS_PROXY"
-  integration_method = "GET"
+  integration_method = "POST"
   integration_uri    = aws_lambda_function.lambda.invoke_arn
   
   payload_format_version = "2.0"
