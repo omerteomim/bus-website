caa95cff54b7dda59e1209b03a96d4653dbbee65
omerteomim <omerteomim@gmail.com>
2025-06-27 19:23:50 +0300

    add api route and stage

--- main.tf
+++ main.tf
@@ -74,6 +74,19 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   payload_format_version = "2.0"
 }
 
+resource "aws_apigatewayv2_route" "example_route" {
+  api_id    = aws_apigatewayv2_api.lambda_api.id
+  route_key = "GET /"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# Deploy Stage
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
 # Lambda permission for API Gateway
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
